05ad92d82cfa634dba60a5d56c052409418bbc02
hexh <250786313@qq.com>
2024-01-31 17:19:36 +0800

    feat: port pretty-ts-errors from source for ansi render

--- scripts/build.js
+++ scripts/build.js
@@ -1,38 +1,94 @@
-(async () => {
-  const ctx = await require("esbuild").context({
-    entryPoints: {
-      extension: "./src/extension.ts",
-    },
-    bundle: true,
-    outdir: "./dist",
-    external: ["vscode"],
-    format: "cjs",
-    inject: ["./scripts/process-shim.js"],
-    tsconfig: "./tsconfig.json",
-    define: process.argv.includes("--production")
-      ? { "process.env.NODE_ENV": '"production"' }
-      : undefined,
-    minify: process.argv.includes("--production"),
-    sourcemap: !process.argv.includes("--production"),
-    plugins: [
-      {
-        name: "node-deps",
-        setup(build) {
-          build.onResolve({ filter: /^path$/ }, (args) => {
-            const path = require.resolve("../node_modules/path-browserify", {
-              paths: [__dirname],
-            });
-            return { path };
-          });
-        },
-      },
-    ],
-  });
-  if (process.argv.includes("--watch")) {
-    await ctx.watch();
-    console.log("watching...");
-  } else {
-    await ctx.rebuild();
-    await ctx.dispose();
+/* eslint-disable @typescript-eslint/no-var-requires */
+const fs = require("fs");
+const path = require("path");
+const chokidar = require("chokidar");
+const esbuild = require("esbuild");
+const child_process = require("child_process");
+const rimraf = require("rimraf");
+
+function getAllTsFiles(dir) {
+  const files = [];
+
+  function traverse(dir) {
+    const entries = fs.readdirSync(dir, { withFileTypes: true });
+
+    for (const entry of entries) {
+      const fullPath = path.join(dir, entry.name);
+
+      if (entry.isDirectory()) {
+        traverse(fullPath);
+      } else if (entry.isFile() && fullPath.endsWith(".ts")) {
+        files.push(fullPath);
+      }
+    }
   }
-})();
+
+  traverse(dir);
+  return files;
+}
+
+async function start() {
+  esbuild
+    .build({
+      entryPoints: getAllTsFiles("src"),
+      bundle: false,
+      format: "esm",
+      tsconfig: "./tsconfig.json",
+      define: process.argv.includes("--production")
+        ? { "process.env.NODE_ENV": '"production"' }
+        : undefined,
+      minify: process.argv.includes("--production"),
+      sourcemap: !process.argv.includes("--production"),
+      mainFields: ["module", "main"],
+      platform: "node",
+      target: "node14.14",
+      outdir: "node_modules/.esbuild-cache/build",
+      plugins: [],
+    })
+    .catch((e) => {
+      console.error(e);
+    })
+    .then(() => {
+      try {
+        child_process.execSync(
+          "rsync -avz --delete ./node_modules/.esbuild-cache/build ./"
+        );
+        console.log("rsync completed successfully.");
+      } catch (error) {
+        console.error("Error occurred while running rsync:", error.message);
+        return;
+      }
+      try {
+        child_process.execSync("npx tsc --build tsconfig.json");
+        console.log("tsc completed successfully.");
+      } catch (e) {
+        console.error("Error occurred while running tsc:", e.message);
+        return;
+      }
+      try {
+        child_process.execSync("yalc push");
+        console.log("yalc pushed successfully.");
+      } catch (e) {}
+    });
+}
+
+function deleteBuild() {
+  return rimraf.sync("./node_modules/.esbuild-cache/build");
+}
+
+deleteBuild();
+start();
+
+if (process.argv.includes("--watch")) {
+  const watcher = chokidar.watch(["src/**/*.*", "yalc.lock"], {
+    ignoreInitial: true,
+  });
+
+  ["change", "add", "unlink"].forEach((event) => {
+    watcher.on(event, (path) => {
+      console.log(`File ${event}: ${path}`);
+      deleteBuild();
+      start();
+    });
+  });
+}
