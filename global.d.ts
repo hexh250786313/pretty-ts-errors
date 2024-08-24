92c30ed924daba5c8b1231e2d650c26aaa37a965
hexh <250786313@qq.com>
2024-08-24 19:39:26 +0800

    feat: add debugger

---
+++ global.d.ts
@@ -0,0 +1,4 @@
+// typescript 定义字符串存在 log 这个方法
+interface String {
+  log(): string;
+}
--- src/format/formatDiagnostic.ts
+++ src/format/formatDiagnostic.ts
@@ -7,10 +7,39 @@ import { prettify } from "./prettify";
 import { format as prettier } from "prettier";
 import { embedSymbolLinks } from "./embedSymbolLinks";
 
+// 接收一个 message 字符串参数，然后追加写入到
+// /home/hexh/workspace/hexh/coc-pretty-ts-errors/.hexh-git-branches/pretty-1/log.txt
+export const log = (message: any) => {
+  if (typeof message !== "string") {
+    message = JSON.stringify(message);
+  }
+  const fs = require("fs");
+  fs.appendFileSync(
+    "/home/hexh/workspace/hexh/coc-pretty-ts-errors/.hexh-git-branches/pretty-1/log.txt",
+    message + "\n"
+  );
+};
+
+
+// 为字符串原型添加 log，便于调试
+String.prototype.log = function () {
+  log(this);
+  return this.toString();
+};
+
+export const clearLog = () => {
+  const fs = require("fs");
+  fs.writeFileSync(
+    "/home/hexh/workspace/hexh/coc-pretty-ts-errors/.hexh-git-branches/pretty-1/log.txt",
+    ""
+  );
+};
+
 export function formatDiagnostic(
   diagnostic: Diagnostic,
   format: (type: string) => string = prettify
 ) {
+  clearLog();
   const result = recoverTypescriptCodeBlockTag(
     embedSymbolLinks(
       prettier(
